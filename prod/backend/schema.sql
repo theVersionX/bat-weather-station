fe1616f8966972c065f663eb583965f905a2efc5
exoProductions <gut.dominic@gmx.ch>
2024-04-30 10:58:24 +0200

    db structure and sql Trigger

--- prod/backend/schema.sql
+++ prod/backend/schema.sql
@@ -1,3 +1,6 @@
+
+//databases and tables------------------------------------------
+
 use avuwaker_exoProductions;
 
 create table weather_station_data_tbl (
@@ -17,5 +20,12 @@ create table weather_station_hardware_tbl (
 
 create table weather_station_wind_warning_tbl(
     id bigint AUTO_INCREMENT PRIMARY KEY,
+    timestamp timestamp,
     isCritical boolean
 );
+
+//triggers---------------------------------------------------------
+//Trigger auf weather_station_data_tbl erstellen (AFTER INSERT)
+
+INSERT INTO weather_station_wind_warning_tbl (windSpeed,isCritical, timestamp)
+VALUES (NEW.windSpeed, CASE WHEN NEW.windSpeed > 50 THEN true ELSE false END, NOW())
